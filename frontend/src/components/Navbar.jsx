f593c8b013362be01597a01de00aa9826bf59738
Aveelash <aveelash.hota04@gmail.com>
2024-11-16 13:56:45 +0530

    Added Cart Page, Place Order page, orders page and login page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
     const [visible, setVisible] = useState(false)
 
-    const { setShowSearch } = useContext(ShopContext)
+    const { setShowSearch, getCartCount } = useContext(ShopContext)
 
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
@@ -41,7 +41,7 @@ const Navbar = () => {
                 <img onClick={() => setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
 
                 <div className='group relative'>
-                    <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
+                    <Link to='/login'><img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" /></Link>
                     <div className='group-hover:block hidden absolute right-0 pt-4'>
                         <div className='gap-2 w-36 bg-slate-100 py-3 px-5 text-gray-500 rounded'>
                             <p className='cursor-pointer hover:text-black'>My Profile</p>
@@ -52,7 +52,7 @@ const Navbar = () => {
                 </div>
                 <Link to='/cart' className='relative'>
                     <img src={assets.cart_icon} className='w-5' alt="" />
-                    <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center bg-black text-white rounded-full leading-4 aspect-square text-[8px]'>10</p>
+                    <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center bg-black text-white rounded-full leading-4 aspect-square text-[8px]'>{getCartCount()}</p>
                 </Link>
                 <img onClick={() => setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
             </div>
